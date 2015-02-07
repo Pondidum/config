03889c25b71db80a1a14a0a13d2dc37bc93e0a94
andy <andy@andydote.co.uk>
2015-02-07 19:33:15 +0000

    configs: add sublimetext3 settings

--- push.sh
+++ push.sh
@@ -2,3 +2,4 @@
 
 cp "configs/.gitconfig" ~
 cp "configs/Default (Windows).sublime-keymap" ~/AppData/Roaming/Sublime\ Text\ 3/Packages/User/
+cp "configs/Preferences.sublime-settings" ~/AppData/Roaming/Sublime\ Text\ 3/Packages/User/

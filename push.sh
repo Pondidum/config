452446800101c484fc1fe12c64203a68bc6e14ab
andy <andy@andydote.co.uk>
2015-05-25 18:58:13 +0100

    remove old shell script

--- push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp "configs/.gitconfig" ~
-cp "configs/Default (Windows).sublime-keymap" ~/AppData/Roaming/Sublime\ Text\ 3/Packages/User/
-cp "configs/Preferences.sublime-settings" ~/AppData/Roaming/Sublime\ Text\ 3/Packages/User/
